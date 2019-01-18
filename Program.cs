e5fe8cb3699e16656b7b7c31875ea04154579c34
Dawid Janczyk <45544015+DawidJanczyk@users.noreply.github.com>
2019-01-18 14:24:31 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100100_4_nie_810
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = -100; i <= 100; i++)
+            {
+                if ((i % 8 != 0) && (i % 10 != 0))
+                {
+                    if (i % 4 != 0) continue;
+                    {
+                        Console.WriteLine("Liczba podzielna przez 4 ale nie podzielne przez 8 i 10 " + i);
+
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
